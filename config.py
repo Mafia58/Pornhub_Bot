dde60cc4020494e98b72375a29c72058f6e4369a
Nimesh Akalanka <90953455+Mafia58@users.noreply.github.com>
2022-03-10 07:40:30 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-
-    API_ID = 7656351
-    API_HASH = abafafb965db50ec6120c80e1eaa0721
-    BOT_TOKEN = 2106961527:AAG9yZ_DG8PrUVpueA-jZeqtzdmCyBi9DXs
-    SUDO = 2137847557
